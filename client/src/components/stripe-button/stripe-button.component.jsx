df89779ca7a8d878c08d3cc8984279f1d45c23d2
Jintan09 <sfritsch09@gmail.com>
2020-12-09 19:10:29 +0100

    added new server

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		'pk_test_51Hv0ZAIm0d2vIo9YkXUYU0t3FNYjgeLBcl505vrWBKXPSl7Hr3cfjGbzTcjeh3Ct4CVbAPPc8YFGrm2vmuzvXkWQ007jmgChx6';
+
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'post',
+			data: {
+				amount: priceForStripe,
+				token: token,
+			},
+		})
+			.then((response) => {
+				alert('Payment successful');
+			})
+			.catch((error) => {
+				console.log('Payment error: ', error);
+				alert(
+					'There was an issue with your payment. Please make sure you use the provided credit card.'
+				);
+			});
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="https://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
---
+++ client/src/pages/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import { Route } from 'react-router-dom';
+import CollectionPageContainer from '../collection/collection.container';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { connect } from 'react-redux';
+
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+	useEffect(() => {
+		fetchCollectionsStart();
+	}, [fetchCollectionsStart]);
+	return (
+		<div className="shop-page">
+			<Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+			<Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+		</div>
+	);
+};
+
+const mapDispatchToProps = (dispatch) => ({
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
