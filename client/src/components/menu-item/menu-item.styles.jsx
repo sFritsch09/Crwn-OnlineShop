93819536253ea66a1f5d2390e7db9acbb3d10c8d
Jintan09 <sfritsch09@gmail.com>
2020-12-10 22:08:55 +0100

    added mobile styles

--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -26,43 +26,47 @@ export const MenuItemContainer = styled.div`
 	}
 
 	&:first-child {
-    margin-right: 7.5px;
-  }
+		margin-right: 7.5px;
+	}
 
-  &:last-child {
-    margin-left: 7.5px;
-  }
+	&:last-child {
+		margin-left: 7.5px;
+	}
+
+	@media screen and (max-width: 800px) {
+		height: 200px;
+	}
 `;
 
 export const BackgroundImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+	width: 100%;
+	height: 100%;
+	background-size: cover;
+	background-position: center;
+	background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
 
 export const ContentContainer = styled.div`
-  height: 90px;
-  padding: 0 25px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  background-color: white;
-  opacity: 0.7;
-  position: absolute;
+	height: 90px;
+	padding: 0 25px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	border: 1px solid black;
+	background-color: white;
+	opacity: 0.7;
+	position: absolute;
 `;
 
 export const ContentTitle = styled.span`
-  font-weight: bold;
-  margin-bottom: 6px;
-  font-size: 22px;
-  color: #4a4a4a;
+	font-weight: bold;
+	margin-bottom: 6px;
+	font-size: 22px;
+	color: #4a4a4a;
 `;
 
 export const ContentSubtitle = styled.span`
-  font-weight: lighter;
-  font-size: 16px;
+	font-weight: lighter;
+	font-size: 16px;
 `;
